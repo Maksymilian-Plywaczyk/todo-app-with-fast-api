b4fc0bf083e576befdeaab2a3260ebdab30e09f0
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-11-23 22:26:37 +0100

    Add security.py with contains JWT authentication

---
+++ app/core/security.py
@@ -0,0 +1,38 @@
+from passlib.context import CryptContext
+import os
+from typing import Union
+from datetime import datetime, timedelta
+from jose import jwt
+
+# get secret key from environment variables should be kept secret
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+ALGORITH = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# takes the plain password and returns the hash for it (stored in database)
+def get_hashed_password(plain_password: str) -> str:
+    return password_context.hash(plain_password)
+
+
+# takes the plain and hashed passwords and validate them
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(subject: Union[str, any], expires_delta: int = None) -> str:
+
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "subject": str(subject)}
+    encoded_jwt = jwt.encode(
+        claims=to_encode, key=SECRET_KEY, algorithm=ALGORITH)
+    return encoded_jwt
