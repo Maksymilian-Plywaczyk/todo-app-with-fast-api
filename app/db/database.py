38cf64587cb632856b1a5071d3c8a4fba1421593
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-11-23 23:19:03 +0100

    Add configuration of database

---
+++ app/db/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+SQLALCHEMY_DATBASE_URL = os.getenv("SQLALCHEMY_DATBASE_URL")
+
+# argument connect_args is needed only for SQLite databases.
+engine = create_engine(url=SQLALCHEMY_DATBASE_URL,
+                       connect_args={"check_same_thread": True})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
