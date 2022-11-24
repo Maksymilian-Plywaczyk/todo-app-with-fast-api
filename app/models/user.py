882d5089c6d82b07077620fa9b9b4ca76244cd4d
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-11-24 20:39:49 +0100

    Add database user model

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Boolean, Integer, String
+
+from db import database
+
+
+class User(database.Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String)
+    is_active = Column(Boolean)
