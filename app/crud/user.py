3342e83de30d7d29064c702e3310c5cb7310dcff
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-11-25 23:55:53 +0100

    Add user submodule in crud with fucntions: get_user_by_email and create_new_user

---
+++ app/crud/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+import models.user
+from schemas.user import UserCreate
+from core.security import get_hashed_password
+
+
+def get_user_by_email(db: Session, user_email: str):
+    user_by_email = db.query(models.user.User).filter(
+        models.user.User.email == user_email).first()
+    return user_by_email
+
+
+def create_new_user(db: Session, user: UserCreate):
+    hashed_password = get_hashed_password(plain_password=user.password)
+    database_user = models.user.User(
+        full_name=user.full_name, email=user.email, hashed_password=hashed_password)
+    db.add(database_user)
+    db.commit()
+    db.refresh()
+    return database_user
