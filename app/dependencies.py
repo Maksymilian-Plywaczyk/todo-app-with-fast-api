cd2d03b6b301ccd4ff9b26f731deb6908476ea63
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-11-27 16:04:48 +0100

    Create all tables in database

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,7 +4,7 @@ from db.database import SessionLocal
 
 def get_db() -> Generator:
     try:
-        db = SessionLocal
+        db = SessionLocal()
         yield db
     finally:
         db.close()
--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
+from routers import singup
+from models import user
+from db.database import engine
 
+user.database.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.include_router(singup.router)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"message": "Hello Bigger Applications!"}
