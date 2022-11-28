1978de8357d48e99459d27016ec8a8f153e9033e
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-11-28 21:46:03 +0100

    Add TokenPayload class to authorizate token

--- app/schemas/token.py
+++ app/schemas/token.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
+from typing import Union
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenPayload(BaseModel):
+    subject: str = None
+    exp: int = None
