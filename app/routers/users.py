1f3dfbeaeb9d7f47196f725cb683f12d13bf5192
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-12-17 18:55:40 +0100

    Add endpoint for creating new task for user

--- app/routers/users.py
+++ app/routers/users.py
@@ -4,6 +4,8 @@ from schemas.users import User, UserCreate
 from sqlalchemy.orm import Session
 from crud.users import delete_user, get_user_by_email, get_user_list
 from typing import List
+from schemas.tasks import Task, TaskCreate
+from crud.tasks import create_task
 from routers.tags import Tags
 
 router = APIRouter()
@@ -17,6 +19,12 @@ def get_me(current_user: User = Depends(get_current_user)):
 @router.get("/users", summary="Get all users in database", response_model=List[User], tags=[Tags.users])
 def get_users(db: Session = Depends(get_db), skip: int = 0, limit: int = 0):
     return get_user_list(db=db, skip=skip, limit=limit)
+
+
+@router.post("/users/{user_id}/task", summary="Create new task for user", response_model=Task, tags=[Tags.tasks])
+def create_new_task(user_id: int, new_task: TaskCreate, db: Session = Depends(get_db)):
+    task = create_task(db=db, newTask=new_task, user_id=user_id)
+    return task
 
 
 @router.delete("/delete_user", summary="Delete user from database", tags=[Tags.users])
