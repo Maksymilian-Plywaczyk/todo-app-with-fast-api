801cd070d64396251a49b4174652b8d3844a7906
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-12-22 23:24:28 +0100

    Add endpoint for deleting user task by task_id

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -5,7 +5,7 @@ from routers.tags import Tags
 from sqlalchemy.orm import Session
 from dependencies import get_db
 from crud.users import get_user_by_email
-from crud.tasks import get_tasks_list
+from crud.tasks import get_tasks_list, delete_task
 
 router = APIRouter()
 
@@ -20,3 +20,8 @@ def read_user_tasks(user_email: str, db: Session = Depends(get_db), skip: int = 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return get_tasks_list(db=db, skip=skip, limit=limit)
 
+
+@router.delete("/tasks/{task_id}", response_model=Task, summary="Delete user task by id", tags=[Tags.tasks])
+def delete_user_task(task_id: int, db: Session = Depends(get_db)):
+    deleted_task = delete_task(db=db, task_id=task_id)
+    return deleted_task
