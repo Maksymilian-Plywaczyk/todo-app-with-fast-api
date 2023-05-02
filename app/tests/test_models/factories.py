313b0007c6003f1496e32c51d95cec5d8a2cd42f
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-05-02 11:55:23 +0200

    Add factories to test database models

---
+++ app/tests/test_models/factories.py
@@ -0,0 +1,31 @@
+from random import randint
+
+import factory
+
+from app.models.models import Task, User
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    full_name = factory.Faker('name')
+    email = factory.Faker('email')
+    hashed_password = factory.Faker('password')
+    is_active = True
+    tasks = []
+
+
+class TaskFactory(factory.Factory):
+    class Meta:
+        model = Task
+
+    task_title = 'Test task'
+    task_description = 'Test task description'
+    task_piority = randint(1, 4)
+    create_at = factory.Faker('date')
+    finished_at = factory.Faker('date')
+    is_completed = factory.Faker('boolean')
+    user_id = 1
+
+    owner = UserFactory()
