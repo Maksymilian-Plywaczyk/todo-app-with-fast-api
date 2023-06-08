efdf4a6e2fe3ad826a92b578956af43c1b0f7eb1
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-06-08 17:49:41 +0200

    Config Postgres for starting locally and docker-compose

--- app/db/database.py
+++ app/db/database.py
@@ -4,21 +4,26 @@ from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists
 
 load_dotenv(find_dotenv())
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
-
-POSTGRES_DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres/{POSTGRES_DB}"
-)
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 # argument connect_args is needed only for SQLite databases.
 engine = create_engine(POSTGRES_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def create_database_if_not_exists():
+    if not database_exists(POSTGRES_DATABASE_URL):
+        create_database(POSTGRES_DATABASE_URL)
 
 
 @as_declarative()
